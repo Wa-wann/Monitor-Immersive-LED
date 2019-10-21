2e05d0034f517dbca21927a569ec1dd3963b19b8
Kabroc <benjamin.roussilhe@gmail.com>
2019-10-21 22:04:23 +0200

    Match bitrate between python and Arduino and put # as a symbol of EOL

--- src/led_values_sender.py
+++ src/led_values_sender.py
@@ -2,7 +2,7 @@ import serial
 
 
 class LedValuesSender:
-    def __init__(self, arduinoPath: str, bitrate: int = 19200, timeout: int = 1):
+    def __init__(self, arduinoPath: str, bitrate: int = 500000, timeout: int = 1):
         self.arduinoPath = arduinoPath
         self.bitrate = bitrate
         self.timeout = timeout
@@ -14,7 +14,9 @@ class LedValuesSender:
     def sendLedValues(self, ledStringValues):
         for i in range(len(ledStringValues)):
             self.portArduino.write(str.encode(ledStringValues[i]))
-            # print(ledStringValues[i])
+            #print(ledStringValues[i])
+        self.portArduino.write(str.encode('#'))
 
     def read(self):
-        self.portArduino.read()
+        return self.portArduino.read()
+        
